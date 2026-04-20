83262f16db6d1b0df1185c305403cacfc719a3bf
zhengyyyyy-hnu <zhengyuanzhehao@126.com>
2026-04-20 20:25:51 +0800

    build startserver code
    just like code which in your class file
    and change host to my name

--- Server.py
+++ Server.py
@@ -1,3 +1,14 @@
 import socket
 import threading
 
+def start_server():
+    host = "zyzhshost"
+    port = 51233
+    
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    print("Server online ! ")
+
+
