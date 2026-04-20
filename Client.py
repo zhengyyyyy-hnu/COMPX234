f60ce59c9423e7648e6886d3aa4ff1e66041fbe6
zhengyyyyy-hnu <zhengyuanzhehao@126.com>
2026-04-20 23:40:03 +0800

    just finish read txtfile loop not finish

--- Client.py
+++ Client.py
@@ -2,13 +2,20 @@ import socket
 import threading
 import time
 
-def client_task():
+def client_task( test_number):
     # just only one client worktesk
+
+    #origin textfilename
+    Testfilename =f"test-workload\client_{test_number}.txt"
+
+
     client_socket = None
     try :
         client_socket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect(("zyzhshost",51233))
-        client_socket.sendall(Command.encode("utf-8"))
+        with open(Testfilename, "r", encoding="utf-8") as f:
+            for line in f:
+                  client_socket.sendall(line.strip.encode("utf-8"))
         response = client_socket.recv(1024).decode('utf-8')
         print(response)
     except Exception as e:
