d06d1c554f08b60633c55b6eb2d471024dc0c327
zhengyyyyy-hnu <zhengyuanzhehao@126.com>
2026-04-20 20:50:03 +0800

    finish tesk part
    In fact, I haven't completed the part of the instruction yet, but I will finish it later.

--- Client.py
+++ Client.py
@@ -9,3 +9,10 @@ def client_task():
         client_socket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect(("zyzhshost",51233))
         client_socket.sendall(Command.encode("utf-8"))
+        response = client_socket.recv(1024).decode('utf-8')
+        print(response)
+    except Exception as e:
+        print("Server is error")
+    finally:
+        if client_socket:
+            client_socket.close()
