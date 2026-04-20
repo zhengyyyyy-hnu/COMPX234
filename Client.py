f66c290501661151298530da61a8c41bbd33c607
zhengyyyyy-hnu <zhengyuanzhehao@126.com>
2026-04-20 20:45:28 +0800

    a easy part of client
    I don't know for the moment how to deal with the commands in the txt file. I'll just leave it like this for now.

--- Client.py
+++ Client.py
@@ -2,3 +2,10 @@ import socket
 import threading
 import time
 
+def client_task():
+    # just only one client worktesk
+    client_socket = None
+    try :
+        client_socket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(("zyzhshost",51233))
+        client_socket.sendall(Command.encode("utf-8"))
